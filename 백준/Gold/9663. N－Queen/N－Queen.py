886e6493baea6ad9f69cf454670ddebab1fdb080
JeongHanO <129676250+JeongHanO@users.noreply.github.com>
2023-04-11 11:17:46 +0900

    [Gold IV] Title: N-Queen, Time: 30160 ms, Memory: 205120 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,22 +1,22 @@
 n = int(input())
+row = [0]*n
+cnt = 0
 
-pos = [0] * n
-flag_a = [False] * n
-flag_b = [False] * (n*2-1)
-flag_c = [False] * (n*2-1)
-ans = 0
+def rec(x):
+    global cnt
+    if x == n:
+        cnt += 1
+    else:
+        for y in range(n):
+            row[x] = y
+            if check(x):
+                rec(x+1)
 
-def set(i):
-    global ans
-    for j in range(n):
-        if( not flag_a[j] and not flag_b[i+j] and not flag_c[i-j+n-1]):
-            pos[i] = j
-            if i == n-1: 
-                ans += 1
-            else:
-                flag_a[j]= flag_b[i+j]= flag_c[i-j+n-1] = True
-                set(i+1)
-                flag_a[j]= flag_b[i+j]= flag_c[i-j+n-1] = False
+def check(x):
+    for i in range(x):
+        if (row[x] == row[i]) or (abs(row[i]-row[x])==(x-i)):
+            return False
+    return True
 
-set(0)
-print(ans)
+rec(0)
+print(cnt)
