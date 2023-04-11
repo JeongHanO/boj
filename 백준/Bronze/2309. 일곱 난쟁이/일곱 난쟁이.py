c276c933f021643dea2a7f9f234fe2408fbc35b6
JeongHanO <129676250+JeongHanO@users.noreply.github.com>
2023-04-11 09:54:18 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,22 +1,9 @@
+import itertools
+
 arr = [int(input()) for _ in range(9)]
-lst = []
 
-
-
-def search(i,dep):
-    if dep == 7:
-        if sum(lst) == 100:
-            lst.sort()
-            for x in range(7):
-                print(lst[x])
-            exit()
-        else:
-            return
-
-    for j in range(i,9):
-        lst.append(arr[j])
-        search(i+1,dep+1)
-        lst.pop()
-
-
-search(0,0)
+for i in itertools.combinations(arr,7):
+    if sum(i) == 100:
+        for j in sorted(i):
+            [print(j)]
+        break
