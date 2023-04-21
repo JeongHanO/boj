4490f41fafa719e6f9cfed38e203b188f3aab0b4
JeongHanO <129676250+JeongHanO@users.noreply.github.com>
2023-04-21 21:18:52 +0900

    [Silver I] Title: 트리 순회, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,22 +1,5 @@
-# class Node:
-#     def __init__(self, value):
-#         self.data = value
-#         self.left = None
-#         self.right = None
-
-
-# class BTree():
-#     def __init__(self):
-#         self.root = None
-
-
 n = int(input())
 lst = [list(input().split()) for _ in range(n)]
-
-# 배열로 푸는 방법은 없을까...
-# 루트부터 시작해서 자식 노드로 들어감. 더이상 자식 노드가 없으면 리프노드로 판단, 거기에서 탐색 시작
-# 재귀를 통해서 들어오면 return을 통해서 올라가면 됨.
-
 
 def preorder(node):
     print(node[0], end='')
