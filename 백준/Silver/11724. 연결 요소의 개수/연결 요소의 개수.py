a7fbc85dbafc412e73fb8881cfe609f84e5db83a
JeongHanO <129676250+JeongHanO@users.noreply.github.com>
2023-04-22 19:41:46 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 692 ms, Memory: 65008 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,37 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+adj = [[]for _ in range(n+1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    adj[a].append(b)
+    adj[b].append(a)
+
+
+def bfs(v):
+    global cnt
+    if visited[v]:
+        return
+    visited[v] = True
+    q = deque()
+    q.append(v)
+    while q:
+        a = q.pop()
+        for i in adj[a]:
+            if not visited[i]:
+                q.append(i)
+                visited[i] = True
+    cnt += 1
+
+
+visited = [False]*(n+1)
+
+cnt = 0
+for i in range(1, n+1):
+    bfs(i)
+
+print(cnt)
