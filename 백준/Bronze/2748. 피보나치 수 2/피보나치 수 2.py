9715bc223084516b16f1c2b1a8da21134137ea61
JeongHanO <129676250+JeongHanO@users.noreply.github.com>
2023-04-29 17:18:27 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,17 +1,24 @@
-def fib1(n):
+def fib1(n):  # top-down
     if n < 2:
         return n
-    if arr[n]:
-        return arr[n]
+    if arr1[n]:
+        return arr1[n]
     else:
-        arr[n] = fib1(n-1) + fib1(n-2)
-        return arr[n]
+        arr1[n] = fib1(n-1) + fib1(n-2)
+        return arr1[n]
 
 
-def fib2(n):
-    print()
+def fib2(n):  # bottom-up
+    
+    arr2.append(0)
+    arr2.append(1)
+    for i in range(2, n+1):
+        arr2.append(arr2[i-1]+arr2[i-2])
+    return arr2[n]
 
 
 N = int(input())
-arr = [0]*(N+1)
-print(fib1(N))
+arr1 = [0]*(N+1)
+arr2 = []
+# print(fib1(N))
+print(fib2(N))
