cfeedcc5dce0ae32bb285ae1a33143d8009a8053
JeongHanO <129676250+JeongHanO@users.noreply.github.com>
2023-04-30 15:37:24 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,21 +1,8 @@
-import re
-from collections import deque
-
-expression = input()
-numbers = re.findall('\d+', expression) 
-operators = re.findall('\D', expression)
-
-lst = deque(numbers)
-result = []
-result.append(int(lst.popleft()))
-
-for i in operators:
-    if i == '+':
-        result.append(int(result.pop())+int(lst.popleft()))
-    else:
-        result.append(int(lst.popleft()))
-ans = result[0]
-for i in range(1,len(result)):
-    ans -= result[i]
-
-print(ans)
+arr = input().split('-')
+s = 0
+for i in arr[0].split('+'):
+    s += int(i)
+for i in arr[1:]:
+    for j in i.split('+'):
+        s -= int(j)
+print(s)
