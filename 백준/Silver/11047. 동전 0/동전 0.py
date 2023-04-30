f02dd261096fe2077b55572cf02153573c1ca2dd
JeongHanO <129676250+JeongHanO@users.noreply.github.com>
2023-04-30 15:03:47 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -3,14 +3,11 @@ coins = []
 
 for _ in range(n):
     coins.append(int(input()))
-cnt = 0
 
+cnt = 0
 for i in range(n-1, -1,-1):
-    if k == 0:
-        break
     while k >= coins[i]:
-        tmp = k//coins[i]
-        k -= coins[i]*tmp
-        cnt += tmp
+        cnt += k//coins[i]
+        k = k%coins[i]
 
 print(cnt)
