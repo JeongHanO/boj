c1a550e4f9627f26ad2c32b66896f9d524690c63
JeongHanO <129676250+JeongHanO@users.noreply.github.com>
2023-05-02 14:13:05 +0900

    [Silver III] Title: 1로 만들기, Time: 540 ms, Memory: 39068 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -4,14 +4,13 @@ x = int(input())
 cnt = 0
 num = 1
 
-dp = [sys.maxsize]*1000001
-dp[1] = 0
+dp = [0]*(x+1)
 
-for i in range(1,x+1):
+for i in range(2, x+1):
+    dp[i] = dp[i-1]+1
     if i % 3 == 0:
-        dp[i] = min(dp[i//3]+1, dp[i-1]+1)
+        dp[i] = min(dp[i//3]+1, dp[i])
     if i % 2 == 0:
         dp[i] = min(dp[i//2]+1, dp[i])
-    dp[i] = min(dp[i], dp[i-1]+1)
 
 print(dp[x])
