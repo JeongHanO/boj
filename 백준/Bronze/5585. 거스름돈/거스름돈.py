09e9e85363cc15ab688c0eb99a2491f8c54e8c67
JeongHanO <129676250+JeongHanO@users.noreply.github.com>
2023-05-02 10:41:53 +0900

    [Bronze II] Title: 거스름돈, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.py
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -1,11 +1,10 @@
-import sys
-input = sys.stdin.readline
+n = int(input())
+n = 1000-n
 
+cnt = 0
 coins = [500, 100, 50, 10, 5, 1]
-paid_money = int(input())
-temp = 1000 - paid_money
-ans = 0
+
 for coin in coins:
-    ans += temp//coin
-    temp %= coin
-print(ans)
+    cnt += n//coin
+    n %= coin
+print(cnt)
