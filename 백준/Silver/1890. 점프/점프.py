d0043870427b25879352b8a6c6a42038c94f51a8
JeongHanO <129676250+JeongHanO@users.noreply.github.com>
2023-05-04 10:21:55 +0900

    [Silver I] Title: 점프, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+jump = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [[0]*n for _ in range(n)]
+dp[0][0] = 1
+
+for i in range(n):
+    for j in range(n):
+        if dp[i][j] and not (i==n-1 and j==n-1):
+            if i+jump[i][j] < n:
+                dp[i+jump[i][j]][j] += dp[i][j]
+                
+            if j+jump[i][j] < n:
+                dp[i][j+jump[i][j]] += dp[i][j]
+                
+            
+print(dp[-1][-1])
