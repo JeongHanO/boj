1cd3a2efdfd263b50a1c143f5d40d4f1ffb6fd48
JeongHanO <129676250+JeongHanO@users.noreply.github.com>
2023-06-03 09:45:45 +0900

    [Silver V] Title: 수들의 합 5, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.cc
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <math.h>
+using namespace std;
+ 
+int main(void)
+{    
+    int N;
+    int ans = 0;
+        
+    cin >> N;
+    
+    int left = 1;
+    int right = 1;
+    int sum = 0;
+    
+    while(left <= right && right <= N+1)
+    {        
+        if(sum > N)
+        {
+            sum -= left;
+            left++;
+        }
+        else if(sum < N)
+        {
+            sum += right;
+            right++;
+        }
+        else if(sum == N)
+        {
+            ans++;
+            
+            sum += right;
+            right++;
+        }
+    }
+    
+    cout << ans;
+ 
+    return 0;
+}
