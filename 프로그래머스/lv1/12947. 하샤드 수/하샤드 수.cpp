d77e35c556b3c5e69390a35f96c9e2858b30a726
오정한 <129676250+JeongHanO@users.noreply.github.com>
2023-09-11 16:41:32 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    bool answer = false;
+    
+    int tmp = x;
+    int sum = 0;
+    while(tmp > 0){
+        sum += tmp % 10;
+        tmp /= 10;
+    }
+    
+    if(x%sum == 0) answer = true;
+    
+    return answer;
+}
