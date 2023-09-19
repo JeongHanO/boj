a6f6e6e909383a35d13d54ccc0efb78b98fb66af
오정한 <129676250+JeongHanO@users.noreply.github.com>
2023-09-19 17:53:41 +0900

    [unrated] Title: 콜라 문제, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+    while(n >= a){
+        int tmp = (n/a)*b;
+        answer += tmp;
+        n = n%a+tmp;
+    }
+    return answer;
+}
