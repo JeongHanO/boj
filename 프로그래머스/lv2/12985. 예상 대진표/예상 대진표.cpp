486d143d0862392aed62bfb2e969486fbb93ddbd
오정한 <129676250+JeongHanO@users.noreply.github.com>
2023-09-26 16:16:15 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.cpp
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.cpp
@@ -5,22 +5,14 @@ using namespace std;
 int solution(int n, int a, int b)
 {
     int answer = 0;
-
-    vector<int> v(n, 0);
-    v[a-1] = 1;
-    v[b-1] = 1;
     
-    while(true){
-        vector<int> tmp;
+    a--;
+    b--;
+    
+    while(a != b){
+        a /= 2;
+        b /= 2;
         answer++;
-        
-        for(int i = 0; i < v.size(); i+=2){
-            if(v[i] == 1 && v[i+1] == 1) return answer;
-            if(v[i] == 1 || v[i+1] == 1) tmp.push_back(1);
-            else tmp.push_back(0);
-        }
-        v = tmp;
-        
     }
 
     return answer;
