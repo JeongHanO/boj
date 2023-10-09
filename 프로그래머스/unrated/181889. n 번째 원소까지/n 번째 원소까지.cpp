66282bbd76f590c7b62d055fc7d5b2980b3f2832
오정한 <129676250+JeongHanO@users.noreply.github.com>
2023-10-09 21:12:45 +0900

    [unrated] Title: n 번째 원소까지, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num, int n) {
+    vector<int> answer(num.begin(), num.begin()+n);
+    
+    return answer;
+}
