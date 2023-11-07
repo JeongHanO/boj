bd2e2fc4a8696c231b027217d25b70683e6ed483
오정한 <129676250+JeongHanO@users.noreply.github.com>
2023-11-07 16:18:00 +0900

    [Gold IV] Title: LCS 2, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.cc
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -1,38 +1,31 @@
-#include<iostream>
+#include <iostream>
 #include <string>
 #include <algorithm>
 using namespace std;
-string a, b;
-string result;
-int dp[1001][1001] = { 0, };
 
-int main() {
+int dp[1001][1001];
 
-	ios_base::sync_with_stdio(0);
-	cin.tie(0), cout.tie(0);
-	string temp1, temp2;
-	cin >> temp1;
-	cin >> temp2;
-	
-	a = ' ' + temp1;
-	b = ' ' + temp2;
-	
-	
-	for (int i = 1; i < b.size(); i++) {
-		for (int j = 1; j < a.size(); j++) {
-			if (a[j] == b[i]) dp[i][j] = dp[i - 1][j - 1] + 1;
-			else
-				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-		}
-	}
+int main(){
+    string a_1, b_1;
+    cin >> a_1 >> b_1;
 
-	
-	
-	int col = a.size() - 1;
-	int row = b.size() - 1;
+    string a = ' ' + a_1;
+    string b = ' ' + b_1;
 
+    for(int i = 1; i < b.size(); i++){
+        for(int j = 1; j < a.size(); j++){
+            if(a[j] == b[i]){
+                dp[i][j] = dp[i-1][j-1] + 1;
+            } else{
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+    }
+
+    int row = b.size() - 1;
+    int col = a.size() - 1;
+    string s;
 	while (dp[row][col]) {
-
 		if (dp[row][col] == dp[row - 1][col]) {
 			row--;
 		}
@@ -40,16 +33,16 @@ int main() {
 			col--;
 		}
 		else {
-			result += a[col];
+			s += a[col];
 			row--, col--;
 		}
-
 	}
 
-	cout << dp[b.size() - 1][a.size() - 1] << endl;
-	if (result.size() > 0) {
-		reverse(result.begin(), result.end());
-		cout << result << endl;
-	}
-	return 0;
+    cout << dp[b.size()-1][a.size()-1];
+    if(s.size() > 0){
+        reverse(s.begin(), s.end());
+        cout << "\n" << s;
+    }
+
+    return 0;
 }
