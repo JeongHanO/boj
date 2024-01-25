a96371b1d40b8f7023a54095c6c4aecffa3a1576
오정한 <129676250+JeongHanO@users.noreply.github.com>
2024-01-25 16:54:33 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,41 +1,22 @@
-#include <vector>
-#include <algorithm>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int a, b;
-vector<int> va;
-
-void check(int n){
-    int l = 0, r = a-1;
-    int mid;
-    while(l <= r){
-        mid = (l+r)/2;
-        if(n == va[mid]){
-            printf("1\n");
-            return;
-        }
-        if(n < va[mid]) r = mid-1;
-        else l = mid+1;
-    }
-    printf("0\n");
-    return;
-}
+int a[100001];
 
 int main(){
-    cin >> a;
-    va.assign(a, 0);
-    for(int i = 0; i < a; i++){
-        scanf("%d\n", &va[i]);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n, m;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
     }
-
-    sort(va.begin(), va.end());
-
-    cin >> b;
-    for(int i = 0; i < b; i++){
-        int tmp; scanf("%d\n", &tmp);
-        check(tmp);
+    sort(a, a+n);
+    
+    cin >> m;
+    while(m--){
+        int t; cin >> t;
+        cout << binary_search(a, a+n, t) << '\n';
     }
-
-    return 0;
 }
