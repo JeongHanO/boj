335da92e9b64ebfcc93310e5430302221dc17202
오정한 <129676250+JeongHanO@users.noreply.github.com>
2024-05-13 14:20:46 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -1,74 +1,44 @@
 #include <iostream>
 using namespace std;
 
-char arr[51][51];
-char white_arr[8][8] = {
-	'W','B','W','B','W','B','W','B',
-	'B','W','B','W','B','W','B','W',
-	'W','B','W','B','W','B','W','B',
-	'B','W','B','W','B','W','B','W',
-	'W','B','W','B','W','B','W','B',
-	'B','W','B','W','B','W','B','W',
-	'W','B','W','B','W','B','W','B',
-	'B','W','B','W','B','W','B','W'
-};
+bool board[51][51]; 
 
-char black_arr[8][8] = {
-	'B','W','B','W','B','W','B','W',
-	'W','B','W','B','W','B','W','B',
-	'B','W','B','W','B','W','B','W',
-	'W','B','W','B','W','B','W','B',
-	'B','W','B','W','B','W','B','W',
-	'W','B','W','B','W','B','W','B',
-	'B','W','B','W','B','W','B','W',
-	'W','B','W','B','W','B','W','B'
-};
+int main(){
+    int n, m;
+    cin >> n >> m;
+    int ans = 10000000;
 
-int white_first(int x, int y) {
-	int result = 0;
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++) {
-			if (arr[x + i][y + j] != white_arr[i][j])
-				result++;
-		}
-	}
-	return result;
-}
+    for(int x = 0; x < n; x++){
+        for(int y = 0; y < m; y++){
+            char c; cin >> c;
+            if(c == 'W') board[x][y] = true;
+            else board[x][y] = false;
+        }
+    }
 
-int black_first(int x, int y) {
-	int result = 0;
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++) {
-			if (arr[x + i][y + j] != black_arr[i][j])
-				result++;
-		}
-	}
-	return result;
-}
-
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	int n, m, result = 64;
-	cin >> n >> m;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			cin >> arr[i][j];
-
-	int t_white, t_black;
-	for (int i = 0; i <= n - 8; i++) {
-		for (int j = 0; j <= m - 8; j++) {
-			t_white = white_first(i, j);
-			t_black = black_first(i, j);
-			if (t_white < t_black) {
-				result = (t_white < result) ? t_white : result;
-			}
-			else {
-				result = (t_black < result) ? t_black : result;
-			}
-		}
-	}
-	cout << result << '\n';
+    for(int i = 0; i <= n-8; i++){
+        for(int j = 0; j <= m-8; j++){
+            int cnt = 0;
+            bool check = true; // W
+            for(int x = i; x < i+8; x++){
+                for(int y = j; y < j+8; y++){
+                    if(board[x][y] != check) cnt++;
+                    check = !check;
+                }
+                check = !check;
+            }
+            if(cnt < ans) ans = cnt;
+            cnt = 0;
+            check = false; // B
+            for(int x = i; x < i+8; x++){
+                for(int y = j; y < j+8; y++){
+                    if(board[x][y] != check) cnt++;
+                    check = !check;
+                }
+                check = !check;
+            }
+            if(cnt < ans) ans = cnt;
+        }
+    }
+    cout << ans << endl;
 }
