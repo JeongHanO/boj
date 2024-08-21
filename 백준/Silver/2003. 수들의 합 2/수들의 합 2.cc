d564c5a5926a8c082842a2a4910bcb827b331e7c
오정한 <129676250+JeongHanO@users.noreply.github.com>
2024-08-21 14:44:40 +0900

    [Silver IV] Title: 수들의 합 2, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -1,36 +1,60 @@
 #include <iostream>
-#include <vector>
+
 using namespace std;
 
-int main(){
-    int n, m;
-    cin >> n >> m;
-    
-    vector<int> v;
-    for(int i = 0; i < n; i++){
-        int a; cin >> a;
-        v.push_back(a);
-    }
+// M MAX 3억
+int N;
+int M;
+int maps[10001];
 
-    int left = 0;
-    int right = 0;
-    int cnt = 0;
-    int sum = v[0];
-    while(right < n){
-        if(sum < m){
-            sum += v[++right];
-        }
-        else if(sum == m){
-            cnt++;
-            sum += v[++right];
-        }
-        else{
-            sum -= v[left++];
-            if(left > right){
-                right = left;
-                sum = v[left];
-            }
-        }
-    }
-    cout << cnt;
+int main()
+{
+	/*
+	투포인터 세그먼트 윈도우슬라이딩
+	"연속된 구간 힌트" 
+	M이 되는 합
+	*/
+	cin >> N >> M;
+
+
+
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> maps[i];
+	}
+
+
+	/*
+		1. L<N
+		2. R>L 
+	*/
+	int left = 0;
+	int right = 0;
+
+	int sums = maps[0];
+	int cnt = 0;
+	while (true)
+	{
+		if (right > N) break;
+		if (sums == M) {
+			cnt++;
+		}
+		if (sums < M)
+		{
+			right++;
+			sums += maps[right];
+		}
+		else if (sums >= M)
+		{
+			sums -= maps[left];
+			left++;
+		}
+
+	}
+	cout << cnt;
+
+
+
+	
 }
