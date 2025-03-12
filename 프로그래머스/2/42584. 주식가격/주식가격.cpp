ec2737e90c02cd09856f181ed144b7e372cad6e5
오정한 <129676250+JeongHanO@users.noreply.github.com>
2025-03-12 14:07:06 +0900

    [level 2] Title: 주식가격, Time: 27.18 ms, Memory: 26.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -6,14 +6,45 @@ using namespace std;
 vector<int> solution(vector<int> prices) {
     vector<int> answer;
     
-    for (int idx = 0; idx < prices.size(); ++idx){
-        int chk = 0;
-        for(int i = idx+1; i < prices.size(); ++i){
-            chk++;
-            if(prices[idx] > prices[i]) 
-                break;
+    int len = prices.size();
+    
+    vector<int> period(len, -1); // 자신보다 작은 값이 나오는 시점 기록
+    vector<pair<int, int>> stk;
+    
+    stk.push_back({prices[0], 0});
+    
+    for(int i = 1; i < len; i++){
+        if(prices[i] >= stk.back().first){
+            stk.push_back({prices[i], i});
+        }
+        else{
+            while(prices[i] < stk.back().first){
+                period[stk.back().second] = i;
+                stk.pop_back();
+                if(stk.size() == 0){ // 비교 대상 없음
+                    stk.push_back({prices[i],i});
+                    break;
+                }
             }
-        answer.push_back(chk);
+            stk.push_back({prices[i], i});
+        }
     }
+    
+    for(int i = 0; i < len; i++){
+        if(period[i] == -1){
+            answer.push_back(len-i-1);
+        }
+        else{
+            answer.push_back(period[i]-i);
+        }
+    }
+    
     return answer;
 }
+
+/*
+배열에 하나씩 추가하면서 제일 뒤와 숫자 비교.
+커지거나 같으면 추가, 작아지면 pop.
+추가하는 숫자보다 작거나 같은 수 나올 때까지 pop.
+새로운 배열에 pop된 시점 기록.
+*/
