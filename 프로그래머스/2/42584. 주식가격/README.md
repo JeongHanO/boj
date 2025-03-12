d08caaa66f10677742415f23c2e03f1beccf2be6
오정한 <129676250+JeongHanO@users.noreply.github.com>
2025-03-12 14:18:59 +0900

    [level 2] Title: 주식가격, Time: 26.91 ms, Memory: 26.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584#) 
 
 ### 성능 요약
 
-메모리: 26.8 MB, 시간: 27.18 ms
+메모리: 26.7 MB, 시간: 26.91 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 12일 14:07:01
+2025년 03월 12일 14:18:57
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -1,50 +1,27 @@
 #include <string>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
-    vector<int> answer;
+    int size = prices.size();
+    vector<int> answer(size);
+    stack<int> stk;
     
-    int len = prices.size();
-    
-    vector<int> period(len, -1); // 자신보다 작은 값이 나오는 시점 기록
-    vector<pair<int, int>> stk;
-    
-    stk.push_back({prices[0], 0});
-    
-    for(int i = 1; i < len; i++){
-        if(prices[i] >= stk.back().first){
-            stk.push_back({prices[i], i});
+    for(int i = 0; i < size; i++){
+        while(!stk.empty() && prices[stk.top()] > prices[i]){
+            answer[stk.top()] = i - stk.top();
+            stk.pop();
         }
-        else{
-            while(prices[i] < stk.back().first){
-                period[stk.back().second] = i;
-                stk.pop_back();
-                if(stk.size() == 0){ // 비교 대상 없음
-                    stk.push_back({prices[i],i});
-                    break;
-                }
-            }
-            stk.push_back({prices[i], i});
-        }
+        stk.push(i);
     }
     
-    for(int i = 0; i < len; i++){
-        if(period[i] == -1){
-            answer.push_back(len-i-1);
-        }
-        else{
-            answer.push_back(period[i]-i);
-        }
+    while(!stk.empty()){
+        answer[stk.top()] = size - stk.top() - 1;
+        stk.pop();
     }
+    
     
     return answer;
 }
-
-/*
-배열에 하나씩 추가하면서 제일 뒤와 숫자 비교.
-커지거나 같으면 추가, 작아지면 pop.
-추가하는 숫자보다 작거나 같은 수 나올 때까지 pop.
-새로운 배열에 pop된 시점 기록.
-*/
